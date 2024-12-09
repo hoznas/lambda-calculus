6be0f137656275f54c1d209e70bd4ea81927e905
hoznas <s_unkun@yahoo.co.jp>
2024-12-09 19:26:22 +0900

    s

--- blog4.mjs
+++ blog4.mjs
@@ -41,17 +41,7 @@ console.log('====== blog4.mjs ======');
 // const LEFT = (p) => p(TRUE); // 定義済み
 // const RIGHT = (p) => p(FALSE); // 定義済み
 
-const LEFT_OR_ZERO = (halfTape) => {
-  const n = LEFT(halfTape);
-  // console.log(
-  //   'LEFT_OR_ZERO',
-  //   IF(IS_NIL(ZERO))('true')('false'), //TO_INT(LEFT(halfTape))
-  //   TO_INT(n),
-  //   IF(IS_NIL(n))('true')('false')
-  // );
-
-  return IF(IS_NIL(halfTape))(ZERO)(LEFT(halfTape));
-};
+const LEFT_OR_ZERO = (halfTape) => IF(IS_NIL(halfTape))(ZERO)(LEFT(halfTape));
 
 const NEW_TAPE = (left) => (current) => (right) =>
   PAIR(left)(PAIR(current)(right));
@@ -59,41 +49,41 @@ const LEFT_TAPE = LEFT;
 const CURRENT = (tape) => LEFT(RIGHT(tape));
 const RIGHT_TAPE = (tape) => RIGHT(RIGHT(tape));
 
-const TM_INC = (tape) =>
-  NEW_TAPE(LEFT_TAPE(tape))(INC(CURRENT(tape)))(RIGHT_TAPE(tape));
-const TM_DEC = (tape) =>
-  NEW_TAPE(LEFT_TAPE(tape))(DEC(CURRENT(tape)))(RIGHT_TAPE(tape));
-const TM_RIGHT = (tape) =>
-  NEW_TAPE(PAIR(CURRENT(tape))(LEFT_TAPE(tape)))(
+const TM_INC = (tape) => {
+  console.log('TM_INC');
+  return NEW_TAPE(LEFT_TAPE(tape))(INC(CURRENT(tape)))(RIGHT_TAPE(tape));
+};
+const TM_DEC = (tape) => {
+  console.log('TM_DEC');
+  return NEW_TAPE(LEFT_TAPE(tape))(DEC(CURRENT(tape)))(RIGHT_TAPE(tape));
+};
+const TM_RIGHT = (tape) => {
+  console.log('TM_RIGHT');
+  return NEW_TAPE(PAIR(CURRENT(tape))(LEFT_TAPE(tape)))(
     LEFT_OR_ZERO(RIGHT_TAPE(tape))
   )(RIGHT(RIGHT_TAPE(tape)));
+};
 const TM_LEFT = (tape) => {
+  console.log('TM_LEFT');
   const left = RIGHT(LEFT_TAPE(tape));
   const current = LEFT_OR_ZERO(LEFT_TAPE(tape));
   const right = PAIR(CURRENT(tape))(RIGHT_TAPE(tape));
   return NEW_TAPE(left)(current)(right);
 };
 const TM_PRINT = (tape) => {
-  console.log(TO_INT(CURRENT(tape)));
+  console.log('TM_PRINT', TO_INT(CURRENT(tape)));
   return tape;
 };
 
 const FIX2 = (f) => (x) => f(FIX2(f))(x);
 
-const TM_LOOP = (commandList) => {
-  return (tape) => {
-    if (IF(IS_ZERO(CURRENT(tape)))(true)(false)) {
-      return tape;
-    } else {
-      return TM_LOOP(commandList)(EVAL_LIST(commandList, tape));
-    }
-  };
+const TM_LOOP = (commandList) => (tape) => (x) => {
+  console.log('TM_LOOP');
+  const trueCase = (x) => tape;
+  const falseCase = (x) =>
+    TM_LOOP(commandList)(EVAL_LIST(commandList)(tape))(x);
+  return (IF(IS_ZERO(CURRENT(tape)))(true)(false) ? trueCase : falseCase)(x);
 };
-
-const TM_EVALUATE = FIX2((recur) => (tape) => (code) => {
-  console.log('TM_EVALUATE', TO_INT(CURRENT(tape)), LEFT(code));
-  return IF(IS_NIL(code))(tape)(recur(LEFT(code)(tape))(RIGHT(code)));
-});
 
 const tape = NEW_TAPE(NIL)(ZERO)(NIL);
 
@@ -125,104 +115,28 @@ const code2 = toConsList([
   TM_INC,
   TM_LOOP(toConsList([TM_DEC, TM_RIGHT, TM_INC, TM_INC, TM_LEFT])),
   TM_RIGHT,
-  TM_PRINT, // 6
+  TM_PRINT,
 ]);
 
-// let codeList = code1;
-// while (true) {
-//   const cond = IF(IS_NIL(codeList))(true)(false);
-//   console.log({ cond: cond, isNil: IS_NIL(codeList) });
-//   if (cond) break;
-//   // console.log('code', codeList);
-//   const head = LEFT(codeList);
-//   codeList = RIGHT(codeList);
-//   console.log('head', head);
-//   // console.log('loop', head);
-// }
-// console.log('end');
-
-// const LOOP_LIST = (list, tape) => {
-//   let lst = list;
-//   let result = tape;
-//   while (true) {
-//     if (IF(IS_NIL(lst))(true)(false)) break;
-//     const head = LEFT(lst);
-//     console.log('head', head);
-//     result = head(result);
-//     lst = RIGHT(lst);
-//   }
-//   return result;
-// };
-
-// LOOP_LIST(code1, tape);
-
-// const LOOP_LIST2 = (list, tape) => {
-//   let result = tape;
-//   for (let lst = list; !IF(IS_NIL(lst))(true)(false); lst = RIGHT(lst)) {
-//     const head = LEFT(lst);
-//     console.log('head', head);
-//     result = head(result);
-//   }
-//   return result;
-// };
-
-// LOOP_LIST2(code1, tape);
-
-// const LOOP_LIST3 = (list, tape) => {
+// これは正しく動くが、if文を使っているので直したい
+// const EVAL_LIST = (list) => (tape) => {
 //   if (IF(IS_NIL(list))(true)(false)) return tape;
-//   const head = LEFT(list);
-//   console.log('head', head);
-//   return LOOP_LIST3(RIGHT(list), head(tape));
+//   return EVAL_LIST(RIGHT(list))(LEFT(list)(tape));
 // };
 
-// LOOP_LIST3(code1, tape);
-
-// const LOOP_LIST3 = (list, tape) => {
-//   if (IF(IS_NIL(list))(true)(false)) return tape;
-//   const head = LEFT(list);
-//   console.log('head', head);
-//   return LOOP_LIST3(RIGHT(list), head(tape));
-// };
-
-// LOOP_LIST3(code1, tape);
-
-const EVAL_LIST = (list, tape) => {
-  if (IF(IS_NIL(list))(true)(false)) return tape;
-  console.log(LEFT(list));
-  return EVAL_LIST(RIGHT(list), LEFT(list)(tape));
+const EVAL_LIST = (list) => (tape) => (x) => {
+  const trueCase = (x) => tape;
+  const falseCase = (x) => EVAL_LIST(RIGHT(list))(LEFT(list)(tape))(x);
+  console.log('code', LEFT(list));
+  return IF(IS_NIL(list))(true)(false) ? trueCase(x) : falseCase(x);
 };
 
-EVAL_LIST(code2, tape);
+const list = PAIR(ONE)(PAIR(TWO)(PAIR(THREE)(PAIR(FOUR)(NIL))));
 
-// TM_EVALUATE(NEW_TAPE(NIL)(ZERO)(NIL))(code1);
+const sum = Z(
+  (iter) => (list) =>
+    IF(IS_NIL(list))(ZERO)((x) => ADD(LEFT(list))(iter(RIGHT(list)))(x))
+);
+//console.log(sum(NIL)(0));
 
-// [
-//   TM_RIGHT,
-//   TM_INC,
-//   TM_PRINT, // 0 [1]
-//   TM_RIGHT,
-//   TM_INC,
-//   TM_INC,
-//   TM_PRINT, // 0 1 [2]
-//   TM_LEFT,
-//   TM_PRINT, // 0 [1] 2
-//   TM_LEFT,
-//   TM_PRINT, // [0] 1 2
-//   TM_LEFT,
-//   TM_PRINT, // [0] 0 1 2
-//   TM_RIGHT,
-//   TM_RIGHT,
-//   TM_PRINT, // 0 0 [1] 2
-//   TM_RIGHT,
-//   TM_PRINT, // 0 0 1 [2]
-// ].reduce((tape, f) => f(tape), tape);
-
-// [
-//   TM_INC,
-//   TM_INC,
-//   TM_LOOP([TM_DEC, TM_RIGHT, TM_INC, TM_INC, TM_INC, TM_LEFT]),
-//   TM_RIGHT,
-//   TM_PRINT, // 0 [6]
-//   TM_LEFT,
-//   TM_PRINT, // [0] 6
-// ].reduce((tape, f) => f(tape), tape);
+console.log(EVAL_LIST(code2)(tape)('hello'));
